236a9b5d3a32a689a9dd0bfe819945ad66953da3
Benjamin <Benjaminvenezia@hotmail.com>
2022-11-27 15:16:50 +0100

    separate classes

---
+++ public/Background.js
@@ -0,0 +1,17 @@
+"use strict";
+class Background {
+    constructor(game) {
+        this.game = game;
+        this.image1 = document.getElementById("layer1");
+        this.image4 = document.getElementById("layer4");
+        this.layer1 = new Layer(this.game, this.image1, 0.2);
+        this.layer4 = new Layer(this.game, this.image4, 2);
+        this.layers = [this.layer1];
+    }
+    update() {
+        this.layers.forEach((layer) => layer.update());
+    }
+    draw(context) {
+        this.layers.forEach((layer) => layer.draw(context));
+    }
+}
--- public/Game.js
+++ public/Game.js
@@ -7,22 +7,6 @@ window.addEventListener("load", function () {
     canvas.height = 500;
     class Particle {
     }
-    class Background {
-        constructor(game) {
-            this.game = game;
-            this.image1 = document.getElementById("layer1");
-            this.image4 = document.getElementById("layer4");
-            this.layer1 = new Layer(this.game, this.image1, 0.2);
-            this.layer4 = new Layer(this.game, this.image4, 2);
-            this.layers = [this.layer1];
-        }
-        update() {
-            this.layers.forEach((layer) => layer.update());
-        }
-        draw(context) {
-            this.layers.forEach((layer) => layer.draw(context));
-        }
-    }
     class Game {
         constructor(width, height) {
             this.width = width;
